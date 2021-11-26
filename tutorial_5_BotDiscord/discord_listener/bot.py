b285b1694a24682ee50224d719b88ef0505b6b49
Matthieu Vernier <mvernier@inf.uach.cl>
2021-11-26 09:12:21 -0300

    bot discord v2

--- tutorial_5_BotDiscord/discord_listener/bot.py
+++ tutorial_5_BotDiscord/discord_listener/bot.py
@@ -3,7 +3,7 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-
+import threading
 import pika
 
 
@@ -43,6 +43,13 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+    for channel in bot.get_all_channels():
+        print(channel)
+        print(channel.id)
+
+    channel = bot.get_channel(913706828502814760)
+    await channel.send('¡Hola!')
+
 
 @bot.event
 async def on_message(message):
@@ -59,6 +66,7 @@ async def on_message(message):
 async def cumpleaños(ctx):
     message =  ctx.message.content
     print("send a new mesage to rabbitmq: "+message)
+
     channelMQ.basic_publish(exchange='cartero', routing_key="birthday", body=message)
 
 @bot.command(name='add-birthday', help='Permite añadir el cumpleaño de un nuevo miembro de la GUILD que se pasa en parámetro. Ejemplo: !birthday MatthieuVernier 1985-02-13')
@@ -66,12 +74,52 @@ async def cumpleaños(ctx):
     message =  ctx.message.content
     print("send a new mesage to rabbitmq: "+message)
     channelMQ.basic_publish(exchange='cartero', routing_key="birthday", body=message)
-    
 
+############ CONSUMER ###############
+
+import threading
+import asyncio
+
+def writer(bot):
+    """thread worker function"""
+    print('Worker')
+
+    HOST = os.environ['RABBITMQ_HOST']
+
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=HOST))
+    channelMQ = connection.channel()
+
+    #Creamos el exchange 'cartero' de tipo 'fanout'
+    channelMQ.exchange_declare(exchange='cartero', exchange_type='topic', durable=True)
+
+    #Se crea un cola temporaria exclusiva para este consumidor (búzon de correos)
+    result = channelMQ.queue_declare(queue="discord_writer", exclusive=True, durable=True)
+    queue_name = result.method.queue
+
+    #La cola se asigna a un 'exchange'
+    channelMQ.queue_bind(exchange='cartero', queue=queue_name, routing_key="discord_writer")
+
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+
+    async def write(message):
+        channel = bot.get_channel(908505071887732768)#913706828502814760
+        await channel.send(message)
+
+    def callback(ch, method, properties, body):
+        message=body.decode("UTF-8")
+        print(message)
+
+        bot.loop.create_task(write(message))
+
+    channelMQ.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    channelMQ.start_consuming()
+
+t = threading.Thread(target=writer, args=[bot])
+t.start()
+
+########################################
 bot.run(TOKEN)
-
-client = discord.Client()
-channel = client.get_channel(GUILD)
-channel.send("test")
-
-
